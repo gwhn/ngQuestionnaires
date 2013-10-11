a9f3d4fc91befdbd93657399d0bc9ed34dc3f086
gwhn <gwhn@weblitz.co.uk>
2013-10-11 14:03:15 +0100

    Added questionnaire list template

--- app/questionnaires/questionnaireListController.js
+++ app/questionnaires/questionnaireListController.js
@@ -1,6 +1,52 @@
 'use strict';
 
 angular.module('ngQuestionnaires.questionnaireListController', [])
-    .controller('questionnaireListController', [function () {
-
+    .controller('questionnaireListController', ['$scope', function ($scope) {
+        $scope.questionnaires = [
+            {
+                id: 1,
+                title: 'questionnaire 1',
+                description: 'description for questionnaire 1',
+                published: false,
+                questions: [
+                    1,
+                    2,
+                    3
+                ],
+                responses: [
+                    1,
+                    2,
+                    3,
+                    4
+                ]
+            },
+            {
+                id: 2,
+                title: 'questionnaire 2',
+                description: 'description for questionnaire 2',
+                published: true,
+                questions: [
+                    1,
+                    2
+                ],
+                responses: [
+                    1,
+                    2,
+                    3
+                ]
+            },
+            {
+                id: 3,
+                title: 'questionnaire 3',
+                description: 'description for questionnaire 3',
+                published: false,
+                questions: [
+                    1
+                ],
+                responses: [
+                    1,
+                    2
+                ]
+            }
+        ];
     }]);

f99dc0e211058e34fb059616a80b70e143165691
gwhn <gwhn@weblitz.co.uk>
2013-10-12 17:58:09 +0100

    Moved seed data into cache svc and added form for new questionnaire

--- app/questionnaires/questionnaireListController.js
+++ app/questionnaires/questionnaireListController.js
@@ -29,40 +29,7 @@ angular.module('ngQuestionnaires.questionnaireListController', [
         '$scope',
         'cacheService',
         function ($scope, cacheService) {
-            var getQuestionnaires = function () {
-                var qs = cacheService.get('questionnaires');
-                if (qs === undefined) {
-                    qs = [
-                        {
-                            id: 1,
-                            title: 'questionnaire 1',
-                            description: 'description for questionnaire 1',
-                            published: false,
-                            questions: [1, 2, 3],
-                            responses: [1, 2, 3, 4]
-                        },
-                        {
-                            id: 2,
-                            title: 'questionnaire guy 2',
-                            description: 'description for questionnaire 2',
-                            published: true,
-                            questions: [1, 2],
-                            responses: [1, 2, 3]
-                        },
-                        {
-                            id: 3,
-                            title: 'questionnaire 3',
-                            description: 'description for questionnaire 3',
-                            published: false,
-                            questions: [1],
-                            responses: [1, 2]
-                        }
-                    ];
-                    cacheService.put('questionnaires', qs);
-                }
-                return qs;
-            };
-            $scope.questionnaires = getQuestionnaires();
+            $scope.questionnaires = cacheService.get('questionnaires');
 
             $scope.isMatch = function (questionnaire) {
                 return $scope.hasSearchQuery() ? (
--- app/questionnaires/questionnaireNewController.js
+++ app/questionnaires/questionnaireNewController.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('ngQuestionnaires.questionnaireNewController', [])
-    .controller('questionnaireNewController', [function () {
+angular.module('ngQuestionnaires.questionnaireNewController', [
+        'ngQuestionnaires.cacheService'
+    ])
+    .controller('questionnaireNewController', [
+        '$scope',
+        'cacheService',
+        function ($scope, cacheService) {
+            $scope.action = 'New';
 
-    }]);
+            $scope.questions = cacheService.get('questions');
+        }]);

a822d25ed5938dfe5f38116676d7809b24b3aecd
gwhn <gwhn@weblitz.co.uk>
2013-10-13 10:30:55 +0100

    Added new question template with repeating choice sub-form

--- app/questions/questionNewController.js
+++ app/questions/questionNewController.js
@@ -1,4 +1,22 @@
-angular.module('ngQuestionnaires.questionNewController', [])
-    .controller('questionNewController', ['$scope', function ($scope) {
+'use strict';
 
-    }]);
+angular.module('ngQuestionnaires.questionNewController', [
+        'ng'
+    ])
+    .controller('questionNewController', [
+        '$scope',
+        function ($scope) {
+            $scope.action = 'New';
+
+            $scope.question = {
+                choices: []
+            };
+
+            $scope.removeChoice = function (index) {
+                $scope.question.choices.splice(index, 1);
+            };
+
+            $scope.addChoice = function () {
+                $scope.question.choices.push({text: ''});
+            };
+        }]);

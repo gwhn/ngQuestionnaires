95354f5438ec4ad25db68645a17da3dc08cc53a8
gwhn <gwhn@weblitz.co.uk>
2013-10-14 08:14:00 +0100

    Implemented edit question controller

--- app/questions/questionEditController.js
+++ app/questions/questionEditController.js
@@ -1,6 +1,38 @@
 'use strict';
 
 angular.module('ngQuestionnaires.questionEditController', [])
-    .controller('questionEditController', ['$scope', function ($scope) {
+    .controller('questionEditController', [
+        '$scope',
+        '$location',
+        '$routeParams',
+        'fbUrl',
+        'Firebase',
+        'angularFire',
+        function ($scope, $location, $routeParams, fbUrl, Firebase, angularFire) {
+            var ref = new Firebase(fbUrl + 'questions/' + $routeParams.id);
 
-    }]);
+            function navigate() {
+                $location.url('/questions/list');
+            }
+
+            $scope.action = 'Edit';
+
+            angularFire(ref, $scope, 'question');
+
+            $scope.removeChoice = function (index) {
+                $scope.question.choices.splice(index, 1);
+            };
+
+            $scope.addChoice = function () {
+                $scope.question.choices.push({text: ''});
+            };
+
+            $scope.update = function () {
+                ref.set($scope.question);
+                navigate();
+            };
+
+            $scope.cancel = function () {
+                navigate();
+            };
+        }]);

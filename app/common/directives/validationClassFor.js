9a8593ad776cebf666fe96dbf58bf7a182a2c1f7
gwhn <gwhn@weblitz.co.uk>
2013-10-15 22:58:51 +0100

    Added default state to validation class scope watch function

--- app/common/directives/validationClassFor.js
+++ app/common/directives/validationClassFor.js
@@ -6,13 +6,25 @@ angular.module('ngQuestionnaires.validationClassFor', [])
             require: '^form',
             link: function (scope, element, attributes, formController) {
                 scope.$watch(function () {
-                    var field = formController[attributes.validationClassFor];
-                    return field.$invalid && field.$dirty;
-                }, function (value) {
-                    if (value) {
+                    var field = formController[attributes.validationClassFor],
+                        state;
+                    if (field.$invalid && field.$dirty) {
+                        state = 'error';
+                    } else if (field.$valid && field.$dirty) {
+                        state = 'success';
+                    }
+                    return state;
+                }, function (state) {
+                    switch (state) {
+                    case 'error':
                         element.removeClass('has-success').addClass('has-error');
-                    } else {
+                        break;
+                    case 'success':
                         element.removeClass('has-error').addClass('has-success');
+                        break;
+                    default:
+                        element.removeClass('has-error').removeClass('has-success');
+                        break;
                     }
                 });
             }

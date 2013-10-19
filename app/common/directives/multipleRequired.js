c38ff970b92184184d36601d8f24650737bc5f93
gwhn <gwhn@weblitz.co.uk>
2013-10-19 18:32:23 +0100

    Fixed bug with multiple required validation directive

--- app/common/directives/multipleRequired.js
+++ app/common/directives/multipleRequired.js
@@ -6,7 +6,7 @@ angular.module('ngQuestionnaires.multipleRequired', [])
             require: 'ngModel',
             link: function (scope, element, attributes, modelController) {
                 scope.$watch(function () {
-                    return modelController.$modelValue.length;
+                    return modelController.$modelValue && modelController.$modelValue.length;
                 }, function (value) {
                     modelController.$setValidity('multipleRequired', value > 1);
                 });

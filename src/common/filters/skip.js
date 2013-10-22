32e8039757a06bd9c39f31219227c0d2d360aee4
gwhn <gwhn@weblitz.co.uk>
2013-10-22 17:27:24 +0100

    Deploy to gh-pages

--- src/common/filters/skip.js
+++
@@ -1,7 +0,0 @@
-angular.module('ngQuestionnaires.filters')
-
-  .filter('skip', function () {
-    return function (value, start) {
-      return value ? value.slice(parseInt(start, 10)) : value;
-    };
-  });

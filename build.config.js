dab8ad2e9ec9a078d8f78fc12d249c0daf4f305b
gwhn <gwhn@weblitz.co.uk>
2013-10-30 08:23:05 +0000

    Added oauthio component

--- build.config.js
+++ build.config.js
@@ -62,6 +62,7 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/oauthio/app/js/oauth.min.js',
       'vendor/jquery/jquery.min.js',
       'vendor/underscore/underscore-min.js',
       'vendor/bootstrap/dist/js/bootstrap.min.js',

bc22c11a8f92de86a46eed089c9a922e1581eec5
gwhn <gwhn@weblitz.co.uk>
2013-11-13 01:17:34 +0000

    Add resize handler to pie chart

--- src/common/directives/d3PieChart.js
+++ src/common/directives/d3PieChart.js
@@ -1,152 +1,166 @@
 angular.module('ngQuestionnaires.directives')
 
-  .directive('d3PieChart', function (d3) {
-    return {
-      restrict: 'A',
-      scope: {
-        data: '=',
-        label: '@',
-        value: '@',
-        height: '@'
-      },
-      link: function (scope, element, attrs) {
+  .directive('d3PieChart', [
+    '$window',
+    'd3',
+    function ($window, d3) {
+      return {
+        restrict: 'A',
+        scope: {
+          data: '=',
+          label: '@',
+          value: '@',
+          height: '@'
+        },
+        link: function (scope, element, attrs) {
 
-        var chart = d3.select(element[0])
-            .append('svg')
-            .attr('class', 'pie-chart'),
-          height = scope.height || 500,
-          colour = d3.scale.category20(),
-          value = function (d) {
-            return d[scope.value];
-          };
-
-        scope.$watch('data', function (data) {
-          scope.render(data);
-        }, true);
-
-        scope.render = function (data) {
-
-          var width = chart[0][0].offsetWidth,
-            radius = Math.min(height, width) / 2,
-            margin = 30,
-            arc = d3.svg.arc()
-              .startAngle(function (d) {
-                return d.startAngle;
-              })
-              .endAngle(function (d) {
-                return d.endAngle;
-              })
-              .outerRadius(radius - margin)
-              .innerRadius(45),
-            pie = d3.layout.pie().value(value),
-            arcs,
-            total = 0,
-            pieData,
-            label = function (d) {
-              var percentage = (d.data[scope.value] / total) * 100;
-              return percentage.toFixed(1) + "%";
+          var chart = d3.select(element[0])
+              .append('svg')
+              .attr('class', 'pie-chart'),
+            height = scope.height || 500,
+            colour = d3.scale.category20(),
+            value = function (d) {
+              return d[scope.value];
             };
 
-          chart.selectAll('*').remove();
+          $window.onresize = function () {
+            scope.$apply();
+          };
 
-          if (!data || !angular.isArray(data)) {
-            return;
-          }
-
-          // Remove any zero values from data
-          data = data.filter(function (d) {
-            return +d[scope.value] !== 0;
+          scope.$watch(function () {
+            return angular.element($window)[0].innerWidth;
+          }, function () {
+            scope.render(scope.data);
           });
 
-          data.forEach(function (d) {
-            total += d[scope.value];
-          });
+          scope.$watch('data', function (data) {
+            scope.render(data);
+          }, true);
 
-          pieData = pie(data);
+          scope.render = function (data) {
 
-          arcs = chart.attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .selectAll('.arc')
-            .data(pieData)
-            .enter()
-            .append('g')
-            .attr('class', 'arc');
+            var width = chart[0][0].offsetWidth,
+              radius = Math.min(height, width) / 2,
+              margin = 30,
+              arc = d3.svg.arc()
+                .startAngle(function (d) {
+                  return d.startAngle;
+                })
+                .endAngle(function (d) {
+                  return d.endAngle;
+                })
+                .outerRadius(radius - margin)
+                .innerRadius(45),
+              pie = d3.layout.pie().value(value),
+              arcs,
+              total = 0,
+              pieData,
+              label = function (d) {
+                var percentage = (d.data[scope.value] / total) * 100;
+                return percentage.toFixed(1) + "%";
+              };
 
-          arcs.append('path')
-            .attr('d', arc)
-            .attr('fill', function (d, i) {
-              return colour(i);
+            chart.selectAll('*').remove();
+
+            if (!data || !angular.isArray(data)) {
+              return;
+            }
+
+            // Remove any zero values from data
+            data = data.filter(function (d) {
+              return +d[scope.value] !== 0;
             });
 
-          chart.append('g')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .selectAll('line')
-            .data(pieData)
-            .enter()
-            .append('line')
-            .attr('x1', 0)
-            .attr('x2', 0)
-            .attr('y1', -radius + margin - 1)
-            .attr('y2', -radius + margin - 10)
-            .attr('transform', function (d) {
-              return 'rotate(' + (d.startAngle + d.endAngle) / 2 * (180 / Math.PI) + ')';
+            data.forEach(function (d) {
+              total += d[scope.value];
             });
 
-          chart.append('g')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .selectAll('text')
-            .data(pieData)
-            .enter()
-            .append('text')
-            .attr("dy", function (d) {
-              if ((d.startAngle + d.endAngle) / 2 > Math.PI / 2 && (d.startAngle + d.endAngle) / 2 < Math.PI * 1.5) {
-                return 5;
-              } else {
-                return -7;
-              }
-            })
-            .attr("transform", function (d) {
-              return "translate(" + Math.cos(((d.startAngle + d.endAngle - Math.PI) / 2)) * (radius - margin + 12) + "," +
-                Math.sin((d.startAngle + d.endAngle - Math.PI) / 2) * (radius - margin + 12) + ")";
-            })
-            .attr("text-anchor", function (d) {
-              if ((d.startAngle + d.endAngle) / 2 < Math.PI) {
-                return "beginning";
-              } else {
-                return "end";
-              }
-            })
-            .attr('fill', function (d, i) {
-              return colour(i);
-            })
-            .text(function (d) {
-              return d.data[scope.label];
-            });
+            pieData = pie(data);
 
-          arcs.append('text')
-            .attr('transform', function (d) {
-              return 'translate(' + arc.centroid(d) + ')';
-            })
-            .attr('dy', '0.33em')
-            .attr('text-anchor', 'middle')
-            .text(label);
+            arcs = chart.attr('height', height)
+              .append('g')
+              .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+              .selectAll('.arc')
+              .data(pieData)
+              .enter()
+              .append('g')
+              .attr('class', 'arc');
 
-          chart.append('text')
-            .attr('class', 'label')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .attr('dy', -5)
-            .attr('text-anchor', 'middle')
-            .text('TOTAL');
+            arcs.append('path')
+              .attr('d', arc)
+              .attr('fill', function (d, i) {
+                return colour(i);
+              });
 
-          chart.append('text')
-            .attr('class', 'total')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-            .attr('dy', 20)
-            .attr('text-anchor', 'middle')
-            .text(total);
-        };
+            chart.append('g')
+              .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+              .selectAll('line')
+              .data(pieData)
+              .enter()
+              .append('line')
+              .attr('x1', 0)
+              .attr('x2', 0)
+              .attr('y1', -radius + margin - 1)
+              .attr('y2', -radius + margin - 10)
+              .attr('transform', function (d) {
+                return 'rotate(' + (d.startAngle + d.endAngle) / 2 * (180 / Math.PI) + ')';
+              });
 
-      }
-    };
-  });
+            chart.append('g')
+              .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+              .selectAll('text')
+              .data(pieData)
+              .enter()
+              .append('text')
+              .attr("dy", function (d) {
+                if ((d.startAngle + d.endAngle) / 2 > Math.PI / 2 && (d.startAngle + d.endAngle) / 2 < Math.PI * 1.5) {
+                  return 5;
+                } else {
+                  return -7;
+                }
+              })
+              .attr("transform", function (d) {
+                return "translate(" + Math.cos(((d.startAngle + d.endAngle - Math.PI) / 2)) * (radius - margin + 12) + "," +
+                  Math.sin((d.startAngle + d.endAngle - Math.PI) / 2) * (radius - margin + 12) + ")";
+              })
+              .attr("text-anchor", function (d) {
+                if ((d.startAngle + d.endAngle) / 2 < Math.PI) {
+                  return "beginning";
+                } else {
+                  return "end";
+                }
+              })
+              .attr('fill', function (d, i) {
+                return colour(i);
+              })
+              .text(function (d) {
+                return d.data[scope.label];
+              });
+
+            arcs.append('text')
+              .attr('transform', function (d) {
+                return 'translate(' + arc.centroid(d) + ')';
+              })
+              .attr('dy', '0.33em')
+              .attr('text-anchor', 'middle')
+              .text(label);
+
+            chart.append('text')
+              .attr('class', 'label')
+              .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+              .attr('dy', -5)
+              .attr('text-anchor', 'middle')
+              .text('TOTAL');
+
+            chart.append('text')
+              .attr('class', 'total')
+              .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+              .attr('dy', 20)
+              .attr('text-anchor', 'middle')
+              .text(total);
+          };
+
+        }
+      };
+    }
+  ]);
